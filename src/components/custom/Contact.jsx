efe8d7ccb7cd52f0aa1fe1e7aa0bedae0a4aa527
abhinav <abhinavisgreat18@gmail.com>
2024-12-20 17:45:11 +0530

    updated contact buttons

--- src/components/custom/Contact.jsx
+++ src/components/custom/Contact.jsx
@@ -3,14 +3,27 @@ import NodeImage from '../../assets/images/Nodejs.png'
 import ExpressImage from '../../assets/images/Expressjs.png'
 import ReactImage from '../../assets/images/Reactjs.png'
 import MongoDbImage from '../../assets/images/Mongodb.png'
+import { Tooltip } from 'react-tooltip'
 
 const Contact = () => {
+  const handleEmail = () =>{
+    
+    navigator.clipboard.writeText('abhinavisgreat18@gmail.com')
+    .then(()=>alert('Email Address Copied!'))
+  }
   return (
     <div className="mt-10 flex items-start justify-between">
       <div className="flex gap-5">
-        <LinkedinLogo className="cursor-pointer size-7 hover:text-blue-500 " size={32} />
-        <GithubLogo className="cursor-pointer size-7 hover:text-green-500" size={32} />
-        <At className="cursor-pointer size-7 hover:text-red-500" size={32} />
+        <a target="_blank" href='https://www.linkedin.com/in/abhinav-kumar-mernstack/' >
+          <LinkedinLogo className="cursor-pointer size-7 hover:text-blue-500 " size={32} />
+        </a>
+        <a target="_blank" href="https://github.com/Abhinav-not-found">
+          <GithubLogo className="cursor-pointer size-7 hover:text-green-500" size={32} />
+        </a>
+        <button onClick={handleEmail} data-tooltip-id="my-tooltip" data-tooltip-content="Copy Email" >
+          <Tooltip id="my-tooltip" />
+          <At className="cursor-pointer size-7 hover:text-red-500" size={32} />
+        </button>
       </div>
       <div className="flex gap-5 items-center">
         <div className="flex gap-0 items-center font-poppins bg-stone-50 hover:bg-stone-100 border border-gray-200 hover:border-gray-400 pr-2 py-1 rounded-md">
